f40db736e733eac2a73ccafde4be7a1b1327999e
Edward Marion <edmarion6@gmail.com>
2022-05-17 11:22:14 -0400

    commit tree a2d5a32b24098f4c2d124ff5b099e9a8d0c8edad

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+let num1 = 62;
+let num2 = 1;
+
+let multiply = num1 * num2;
+
+let random = Math.ceil(Math.random()); 
+
+let max = Math.max(5, 10, 15, 19, 20);
+
+var num3 = 24;
+var num4 = 5;
+    let mod = num3 % num4;
